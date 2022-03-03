537142391f09b25aa836ad310e039f1c9f6b8f75
Jack <winterschladen.j@gmail.com>
2022-03-03 14:52:55 +0000

    Added up method

--- thermostat.js
+++ thermostat.js
@@ -1,7 +1,15 @@
 class Thermostat {
 
+  constructor() {
+    this.temperature = 20
+  };
+
 getTemperature() {
-  return 20
+  return this.temperature
+};
+
+up() {
+  this.temperature += 1
 };
 
 };
--- thermostat.test.js
+++ thermostat.test.js
@@ -3,10 +3,18 @@ const Thermostat = require('./thermostat')
 describe (Thermostat, () => {
 
   let thermostat = new Thermostat
-  
+
   describe ('getTemperature', () => {
     it ('has a default temperature of 20', () => {
       expect(thermostat.getTemperature()).toBe (20)
+    });
+  });
+
+  describe ('up', () => {
+    it ('increases the temperature by 1', () => {
+      thermostat.up()
+      thermostat.up()
+      expect(thermostat.getTemperature()).toBe (22);
     });
   });
 });
